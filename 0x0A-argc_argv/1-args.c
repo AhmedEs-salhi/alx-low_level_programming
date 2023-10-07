16c02fd9468f4dfe761f56ecd31db5e53b761bd8
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2023-10-07 18:03:11 +0100

    My commit

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -1,15 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * main - the main programe function
+ * @argc: how much words written in the command prompt
+ * @argv: a one dimentional array
+ *
+ * Return: returns the number of the words written in
+ *         the command prompt
+ */
+
 int main(int argc, char *argv[])
 {
 	int i = 0;
 
-	while(i <= argc)
+	while (i <= argc)
 	{
 		i++;
 	}
 	printf("%d\n", argc - 1);
-	
-	return 0;
+
+	return (0);
 }
