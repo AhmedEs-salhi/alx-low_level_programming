5e34be3fada11a43766ff0ea83328f8f763e4882
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2023-10-07 22:40:13 +0100

    my commit

---
+++ 0x0A-argc_argv/3-mult.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - The main function
+ * @argc: the counter argument
+ * @argv: the vector argment
+ *
+ * Return: returns the multiplication
+ *         of the arguments received
+ */
+
+int main(int argc, char *argv[])
+{
+	int i, mult;
+
+	mult = 1;
+
+	if (argc > 1)
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mult *= atoi(argv[i]);
+		}
+		printf("%d\n", mult);
+	}
+	else
+	{
+		printf("Error\n");
+	}
+	return (0);
+}
