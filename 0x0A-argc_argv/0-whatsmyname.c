5bf57554c43ca38480e565de827fe88e002d8ab1
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2024-01-12 20:06:06 +0100

    my commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - the programe function
+ * @argc: how much words written in the command prompt
+ * @argv: a one dimentional array
+ *
+ * Return: returns the program name
+ *
+ */
+
+int main(__attribute__((unused)) int argc, char *argv[])
+{
+	printf("%s\n", argv[0]);
+	return 0;
+}
+
