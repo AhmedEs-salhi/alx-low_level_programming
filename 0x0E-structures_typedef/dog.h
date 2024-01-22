22e4687160993baac4d00bd3975856edc6ff1681
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2024-01-22 23:50:23 +0100

    my commit

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,7 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
-struct dog 
+/**
+ * struct dog - dog structure
+ * @name: the dog's name
+ * @age: the dog's age
+ * @owner: the dog's owner
+ *
+ */
+
+struct dog
 {
 	char *name;
 	float age;
