1b270427b6ff7907e84ae6ffac4e25c32162a445
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2024-01-25 19:12:17 +0100

    my commit

--- 0x0F-function_pointers/2-int_index.c
+++ 0x0F-function_pointers/2-int_index.c
@@ -13,23 +13,15 @@
 
 int int_index(int *array, int size, int (*cmp)(int))
 {
-	int result, i, j;
-
-	j = 0;
 	if (array && cmp)
 	{
 		if (size <= 0)
 			return (-1);
 
-		for (i = 0; i < size; i++)
+		for (int i = 0; i < size; i++)
 		{
-			result = cmp(array[i]);
-			if (result == 1)
+			if (cmp(array[i]))
 				return (i);
-			if (result != 1)
-				j++;
 		}
-		if (j == 20)
-			return (-1);
 	}
 }
