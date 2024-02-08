c9a88836328544764ba8553dc309e997995e9a8a
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2024-02-09 00:09:55 +0100

    my commit

--- 0x14-bit_manipulation/0-binary_to_uint.c
+++ 0x14-bit_manipulation/0-binary_to_uint.c
@@ -1,27 +1,71 @@
 #include "main.h"
+#include <stdlib.h>
 
 /**
- * binary_to_uint - convert binary to decimal
- *
- * @b: pointer to a character
- *
- * Return: the decimal number
- *
+ * _strlen - gets the lenght of input strings
+ * @str: input string
+ * Return: len of the string
+ */
+
+int _strlen(const char *str)
+{
+	int len = 0;
+
+	while (*str)
+	{
+		len++;
+		str++;
+	}
+	return (len);
+}
+
+/**
+ * _to_power - prints the power of numbers
+ * @base: base parameter
+ * @power: power parameter
+ * Return: product of two numbers
+ */
+
+int _to_power(const int base, int power)
+{
+	int product = 1;
+
+	while (power > 1)
+	{
+		product *= base;
+		power++;
+	}
+	return (product);
+}
+
+/**
+ * binary_to_uint - converts a binary number to an unsigned int
+ * @b: input value
+ * Return: converted number, or 0 if b is NULL
  */
 
 unsigned int binary_to_uint(const char *b)
 {
-	unsigned int sum;
+	int power, num;
 
-	sum = 0;
+	power = _strlen(b) - 1;
+	num = 0;
 	if (!b)
+	{
 		return (0);
-
+	}
 	while (*b)
 	{
-		if (*b != '1' && *b != '0')
+		if (!(*b == '0' || *b == '1'))
+		{
 			return (0);
-		sum = sum * 2 + (*b++ - '0');
+		}
+		if (*b == '1')
+		{
+			num += _to_power(2, power);
+		}
+		b++;
+		power--;
 	}
-	return (sum);
+	return (num);
 }
