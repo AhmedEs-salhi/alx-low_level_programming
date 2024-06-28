df7ca382809b53bc91b9695e56751139b7d0edf7
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2024-06-28 12:58:00 +0100

    my commit

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -11,9 +11,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	unsigned long int i;
 	hash_table_t *table;
 
+	if (size == 0)
+		return (NULL);
+
 	table = malloc(sizeof(hash_table_t));
 	if (table == NULL)
 		return (NULL);
+
 	table->size = size;
 	table->array = malloc(size * sizeof(hash_node_t *));
 	if (table->array == NULL)
@@ -21,7 +25,9 @@ hash_table_t *hash_table_create(unsigned long int size)
 		free(table);
 		return (NULL);
 	}
+
 	for (i = 0; i < size; i++)
 		table->array[i] = NULL;
+
 	return (table);
 }
