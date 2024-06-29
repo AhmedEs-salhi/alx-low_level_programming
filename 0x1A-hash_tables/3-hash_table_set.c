53f7191d4164a39271dbe11e365825739500fa62
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2024-06-29 21:46:10 +0100

    Finally

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -33,13 +33,13 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	if (!new)
 		return (0);
 	new->value = strdup(value);
-	if (!new->value)
+	if (!new->key)
 	{
-		free(new->value);
+		free(new);
 		return (0);
 	}
 	new->key = strdup(key);
-	if (!new->key)
+	if (!new->value)
 	{
 		free(new->key);
 		free(new);
