70104eb860727b58f6e3c625224f4be814eb2732
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2024-07-01 02:06:39 +0100

    my commit

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,5 +1,12 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_print - function that print a hash table
+ *
+ * @ht: the hash table
+ *
+ */
+
 void hash_table_print(const hash_table_t *ht)
 {
 	unsigned long int i;
