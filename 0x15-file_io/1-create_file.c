61b0659a33c7921ede51eda6024916df19d77830
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-03-14 15:40:30 +0000

    After a long time again

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -11,14 +11,26 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	ssize_t _open;
+	ssize_t _open, _creat, _write;
 
 	if (!filename)
 		return (-1);
+	if (!text_content)
+	{
+		_creat = creat(filename, S_IRUSR | S_IWUSR);
+		if (_creat == -1)
+			return (-1);
+		return (1);
+	}
+	_creat = creat(filename, S_IRUSR | S_IWUSR);
+	_open = open(filename, O_CREAT | O_RDWR | O_TRUNC);
+	if ((_creat == -1) | (_open == -1))
+		return (-1);
 
-	creat(filename, S_IRUSR | S_IWUSR);
-	_open = open(filename, O_CREAT | O_RDWR);
-	write(_open, text_content, strlen(text_content));
+	_write = write(_open, text_content, strlen(text_content));
+	if (_write == -1)
+		return (-1);
+
 	close(_open);
 
 	return (1);
