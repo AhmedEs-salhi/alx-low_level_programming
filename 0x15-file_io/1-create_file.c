a43d9f32b535c6cc7e4ef00c8722e316d2953d7d
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-03-14 14:47:44 +0000

    After a long debugging session

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <string.h>
+
+/**
+ * create_file - Creates a file with -rw---- permissions
+ *
+ * @filename: Name of the file to be created
+ * @text_content: content of the file newly created
+ *
+ * Return: 1 in case of success. -1 otherwise
+ */
+int create_file(const char *filename, char *text_content)
+{
+	ssize_t _open, _creat, _write;
+
+	if (!filename)
+		return (-1);
+
+	_creat = creat(filename, S_IRUSR | S_IWUSR);
+	_open = open(filename, O_CREAT | O_RDWR);
+	_write = write(_open, text_content, strlen(text_content));
+	close(_open);
+
+	return (1);
+}
