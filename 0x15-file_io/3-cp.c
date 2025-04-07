4519aa9c95ea8b01faca8a2163f269fdaed22b7b
AhmedEs-salhi <ahmedessalhi331@gmail.com>
2025-04-07 22:16:29 +0100

    Fixing it i think

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -12,29 +12,29 @@
 
 int copy_file(const char *file_from, const char *file_to)
 {
-	int open_from, close_from, read_from;
-	int open_to, write_to, close_to;
-	char *buffer = malloc(sizeof(char) * BUFFER_SIZE);
+	ssize_t open_from, close_from, read_from;
+	ssize_t open_to, write_to, close_to;
+	char *buffer = malloc(sizeof(char) * 1024);
 
 	open_from = open(file_from, O_RDONLY);
 	open_to = open(file_to, O_CREAT | O_WRONLY | O_TRUNC, 0666);
 	if (open_from == -1)
 		return (98);
 
-	while (read_from = read(open_from, buffer, sizeof(buffer)) > 0)
-		write_to = write(open_to, buffer, sizeof(buffer));
+	while ((read_from = read(open_from, buffer, sizeof(buffer))) > 0)
+		write_to = write(open_to, buffer, read_from);
 	if (open_to == -1 || write_to == -1)
 		return (99);
 	close_from = close(open_from);
 	if (close_from == -1)
 	{
-		dprintf(2, "Error: Can't close the fd %d\n", open_from);
+		dprintf(2, "Error: Can't close the fd %ld\n", open_from);
 		return (100);
 	}
 	close_to = close(open_to);
 	if (close_to == -1)
 	{
-		dprintf(2, "Error: Can't close the fd %d\n", open_to);
+		dprintf(2, "Error: Can't close the fd %ld\n", open_to);
 		return (100);
 	}
 
